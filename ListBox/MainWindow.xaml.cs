a0e416a772f88e1a66dd94d97f0f82a885b07b30
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-11-29 20:00:49 +0700

    HW3_Notepad DONE

---
+++ ListBox/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace ListBox
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			string[] files = Directory.EnumerateFiles("C:\\Windows").ToArray();
+			foreach (string file in files)
+			{
+				listBox.Items.Add(file);
+			}
+		}
+
+		private void btnAdd_Click(object sender, RoutedEventArgs e)
+		{
+			if(!listBox.Items.Contains(txtInput.Text))
+				listBox.Items.Add(txtInput.Text);
+		}
+
+		private void btnDel_Click(object sender, RoutedEventArgs e)
+		{
+			while(listBox.SelectedItems.Count > 0)
+				listBox.Items.Remove(listBox.SelectedItem);
+		}
+
+		private void btnClr_Click(object sender, RoutedEventArgs e)
+		{
+			listBox.Items.Clear();
+		}
+	}
+}
---
+++ WPF_HW3_Notepad/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_HW3_Notepad
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private string currentFilePath = string.Empty;
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnNew_Click(object sender, RoutedEventArgs e)
+		{
+			txtEditor.Text = string.Empty;
+			currentFilePath = string.Empty;
+			Title = "Notepad - New file";
+		}
+
+		private void btnOpen_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog
+			{
+				Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+			};
+			if(ofd.ShowDialog()==true)
+			{
+				currentFilePath = ofd.FileName;
+				txtEditor.Text = File.ReadAllText(currentFilePath);
+				Title = $"Notepad - {System.IO.Path.GetFileName(currentFilePath)}";
+			}
+		}
+
+		private void btnSave_Click(object sender, RoutedEventArgs e)
+		{
+			if(string.IsNullOrEmpty(currentFilePath))
+				btnSaveAs_Click(sender, e);
+			else
+			{
+				File.WriteAllText(currentFilePath, txtEditor.Text);
+				MessageBox.Show("File saved!", "Saving", MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+		}
+
+		private void btnSaveAs_Click(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog sfd = new SaveFileDialog
+			{
+				Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+			};
+			if(sfd.ShowDialog()==true)
+			{
+				currentFilePath = sfd.FileName;
+				File.WriteAllText (currentFilePath, txtEditor.Text);
+				Title = $"Notepad - {System.IO.Path.GetFileName(currentFilePath)}";
+				MessageBox.Show("File saved!", "Saving", MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+		}
+
+		private void btnExit_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
